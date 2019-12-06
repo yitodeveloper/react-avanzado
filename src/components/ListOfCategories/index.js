3c215ab9c6abfa7e5054cdee8d57d6bac71655d5
Rodrigo Ramírez <yitodeveloper@gmail.com>
2019-12-06 14:20:51 -0300

    agrega hook más fetch desde api externa

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-two-amber.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
       {
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,4 @@
-import styled, { keyframes, css } from 'styled-components'
+import styled from 'styled-components'
 import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
