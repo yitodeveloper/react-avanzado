804cedd3393c1e4e6981066260643550757be1f5
Rodrigo Ramírez <yitodeveloper@gmail.com>
2019-12-09 12:05:40 -0300

    Detecta scroll y muestra lista de categorias fixed

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://petgram-server-two-amber.now.sh/categories')
@@ -12,11 +13,33 @@ export const ListOfCategories = () => {
         setCategories(response)
       })
   }, [])
-  return (
-    <List>
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
